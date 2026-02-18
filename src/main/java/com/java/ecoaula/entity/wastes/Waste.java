8cc3ae8d497752022b8dd25691e06839c8613fa3
MarquesitoClase <juan.luis.marquezcan@gmail.com>
2026-02-18 16:33:44 +0100

    fix: added lombok and jakarta imports.

--- src/main/java/com/java/ecoaula/entity/wastes/Waste.java
+++ src/main/java/com/java/ecoaula/entity/wastes/Waste.java
@@ -1,9 +1,17 @@
 package com.java.ecoaula.entity.wastes;
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.Data;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
 public class Waste{
-    private int id;
+    private Integer id;
     private String name;
-    private Boolean tipe;//true organico
+    private Boolean organic;//true organico
     private String description;
     private float heavy;
 }
